50b7c36664e4f8765a2432ec2ba10e27379c2b5d
Ludea <ludovicw35@hotmail.com>
2022-11-23 15:32:17 +0100

    delete old fn

--- src/utils/Storage.ts
+++ src/utils/Storage.ts
@@ -2,7 +2,6 @@
 import { Stronghold, Location } from "tauri-plugin-stronghold-api/webview-dist";
 
 const stronghold = new Stronghold(".config", "password");
-const store = stronghold.getStore("SparusStore", []);
 const location = Location.generic("vault", "record");
 const clientPath = "sparus";
 
