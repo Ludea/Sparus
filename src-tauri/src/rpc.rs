016336b9e510ea1d366d333cad8944a349043875
Ludea <ludovicw35@hotmail.com>
2023-07-06 10:45:29 +0000

    fix: receive message from server

--- src-tauri/src/rpc.rs
+++ src-tauri/src/rpc.rs
@@ -1,15 +1,14 @@
-pub mod pb {
-  tonic::include_proto!("grpc.examples.echo");
+pub mod luclerpc {
+  tonic::include_proto!("luclerpc");
 }
 
-use pb::{echo_client::EchoClient, Empty};
+use luclerpc::{lucle_client::LucleClient, Empty};
 use tokio_stream::StreamExt;
 use tonic::transport::Channel;
 
-async fn streaming_echo(client: &mut EchoClient<Channel>, num: usize) {
+async fn streaming_echo(client: &mut LucleClient<Channel>, num: usize) {
   let stream = client
     .server_streaming_echo(Empty {
-           // message: "foo".into(),
         })
     .await
     .unwrap()
@@ -22,9 +21,7 @@ async fn streaming_echo(client: &mut EchoClient<Channel>, num: usize) {
 }
 
 pub async fn start_rpc_client() -> Result<(), Box<dyn std::error::Error + Send>> {
-  //let mut client =
-  if let Ok(mut client) = EchoClient::connect("http://127.0.0.1:50051").await {
-    println!("\r\nBidirectional stream echo:");
+  if let Ok(mut client) = LucleClient::connect("http://127.0.0.1:50051").await {
     streaming_echo(&mut client, 17).await;
   }
   Ok(())
