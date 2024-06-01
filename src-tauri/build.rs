3a5ba2c88692e3d5571967ab49f6abdd69856e36
Ludea <ludealol.olol@gmail.com>
2024-06-01 13:27:07 +0200

    fmt

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -4,9 +4,9 @@ use std::{env, fs, path::PathBuf};
 fn main() -> Result<(), Box<dyn std::error::Error>> {
   let file_descriptors = protox::compile(["proto/echo.proto"], ["."]).unwrap();
 
-  let file_descriptor_path = PathBuf::from(env::var_os("OUT_DIR").expect("OUT_DIR not set"))
-        .join("file_descriptor_set.bin");
-    fs::write(&file_descriptor_path, file_descriptors.encode_to_vec()).unwrap();
+  let file_descriptor_path =
+    PathBuf::from(env::var_os("OUT_DIR").expect("OUT_DIR not set")).join("file_descriptor_set.bin");
+  fs::write(&file_descriptor_path, file_descriptors.encode_to_vec()).unwrap();
 
   tauri_build::build();
   tonic_build::configure()
