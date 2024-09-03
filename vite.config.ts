f20f0a71e67532dcf13cdbc2257420aacc6e93d2
Ludea <ludealol.olol@gmail.com>
2024-09-03 10:10:45 +0200

    fix: use internal-ip to enable HMR on mobile

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,29 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { internalIpV4 } from "internal-ip";
 import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
+
+const mobile = !!/android|ios/.exec(process.env.TAURI_ENV_PLATFORM);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr(), tsconfigPaths()],
+  clearScreen: false,
+  server: {
+    port: 5173,
+    strictPort: true,
+    host: mobile ? "0.0.0.0" : false,
+    hmr: mobile
+      ? {
+          protocol: "ws",
+          host: await internalIpV4(),
+          port: 5173,
+        }
+      : undefined,
+    watch: {
+      // 3. tell vite to ignore watching `src-tauri``
+      ignored: ["**/src-tauri/**"],
+    },
+  },
 });
