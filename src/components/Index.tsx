84af69d70e8001ea95c2d8fb96d55fe892144357
Ludea <ludealol.olol@gmail.com>
2024-10-06 16:29:54 +0200

    fix: use only path api on desktop platforms

--- src/components/Index.tsx
+++ src/components/Index.tsx
@@ -1,7 +1,10 @@
 import Footer from "components/Footer";
 
+// Tauri api
+import { platform } from "@tauri-apps/plugin-os";
+
 function Index() {
-  return <Footer />;
+  return platform() !== "android" || platform() !== "ios" ? <Footer /> : null;
 }
 
 export default Index;
