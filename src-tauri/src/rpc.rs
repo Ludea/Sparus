e4c32dcb62ae169b1332be3121757d510827e131
Ludea <ludealol.olol@gmail.com>
2025-09-03 11:39:15 +0000

    feat: download file on Install event

--- src-tauri/src/rpc.rs
+++ src-tauri/src/rpc.rs
@@ -2,22 +2,59 @@ pub mod sparus {
   tonic::include_proto!("sparus");
 }
 
-use sparus::{event_client::EventClient, Empty};
-use tokio_stream::StreamExt;
+use futures::StreamExt;
+use sparus::{event_client::EventClient, Empty, EventType};
+use tauri_plugin_http::reqwest;
+use tokio::{fs::File, io::AsyncWriteExt};
 use tonic::transport::Channel;
 
-async fn streaming_echo(client: &mut EventClient<Channel>, num: usize) {
-  let stream = client.sparus(Empty {}).await.unwrap().into_inner();
+#[derive(serde::Serialize, Debug)]
+pub enum DownloadError {
+  Httperror(String),
+  Io(String)
+}
 
-  let mut stream = stream.take(num);
-  while let Some(item) = stream.next().await {
-    println!("\treceived: {}", item.unwrap().plugin);
+impl From<std::io::Error> for DownloadError {
+  fn from(err: std::io::Error) -> Self {
+    DownloadError::Io(err.to_string())
   }
 }
 
-pub async fn start_rpc_client(url: String) -> Result<(), Box<dyn std::error::Error + Send>> {
+impl From<reqwest::Error> for DownloadError {
+  fn from(err: reqwest::Error) -> Self {
+    DownloadError::Httperror(err.to_string())
+  }
+}
+
+async fn start_streaming(client: &mut EventClient<Channel>) -> Result<(), DownloadError> {
+  let mut stream = client.sparus(Empty {}).await.unwrap().into_inner();
+
+  while let Ok(Some(item)) = stream.message().await {
+    let event = EventType::try_from(item.event_type);
+    if event == Ok(EventType::Install) {
+      if let Err(err) = download_file("").await {
+        println!("error: {:?}", err);
+      }
+      println!("\treceived: {:?}", item.plugin);
+    }
+  }
+  Ok(())
+}
+
+pub async fn start_rpc_client(url: String) -> Result<(), DownloadError> {
   if let Ok(mut client) = EventClient::connect(url).await {
-    streaming_echo(&mut client, 17).await;
+    start_streaming(&mut client).await?;
+  }
+  Ok(())
+}
+
+async fn download_file(url: &'static str) -> Result<(), DownloadError> {
+  let response = reqwest::get(url).await?;
+  let mut stream = response.bytes_stream();
+  let mut file = File::create("foo").await?;
+  while let Some(chunk) = stream.next().await {
+    let data = chunk?;
+    file.write_all(&data).await?;
   }
   Ok(())
 }
