f353e562b441eae9c8133b9cf8b2a21ef57f586d
Ludea <ludealol.olol@gmail.com>
2025-10-29 12:17:56 +0000

    fix: use FC instead of Lazy and Suspense to load js plugins

--- src/components/Plugins.tsx
+++ src/components/Plugins.tsx
@@ -1,31 +1,19 @@
-import { convertFileSrc } from "@tauri-apps/api/core";
-import {
-  useState,
-  useEffect,
-  Suspense,
-  lazy,
-  LazyExoticComponent,
-  ComponentType,
-} from "react";
+import { useState, FC, useEffect } from "react";
 
 export const Plugins = ({ path }: { path: string }) => {
-  const [Comp, setComponent] =
-    useState<LazyExoticComponent<ComponentType> | null>(null);
+  const [Comp, setComponent] = useState<FC>();
 
   useEffect(() => {
-    const url = convertFileSrc(path);
-    const Lazycomponent = lazy(() =>
-      import(/* @vite-ignore */ url).catch((err: unknown) => {
+    const base_url = "http://localhost:8012/plugins/";
+    import(/* @vite-ignore */ base_url + path)
+      .then((mod) => {
+        setComponent(() => mod.default);
+      })
+      .catch((err: unknown) => {
         console.log(err);
-      }),
-    );
-    setComponent(() => Lazycomponent);
+      });
   }, [path]);
 
-  if (!Comp) return <p>Loading component</p>;
-  return (
-    <Suspense fallback={<p>Plugin Initialisation ! </p>}>
-      <Comp />
-    </Suspense>
-  );
+  if (!Comp) return null;
+  return <Comp />;
 };
