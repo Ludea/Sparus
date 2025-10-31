8398adb7c45aeb24fdc88f965d364e8a22d8cd54
Ludea <ludealol.olol@gmail.com>
2025-10-31 12:27:06 +0000

    fix: disable 2 ts eslint rules

--- src/components/Plugins.tsx
+++ src/components/Plugins.tsx
@@ -7,6 +7,9 @@ export const Plugins = ({ path }: { path: string }) => {
     const base_url = "http://localhost:8012/plugins/";
     import(/* @vite-ignore */ base_url + path)
       .then((mod) => {
+        /* eslint-disable-next-line 
+           @typescript-eslint/no-unsafe-return, 
+           @typescript-eslint/no-unsafe-member-access */
         setComponent(() => mod.default);
       })
       .catch((err: unknown) => {
