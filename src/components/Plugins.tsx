b4d4b99b5819ae224cad8541200e5f11b84b8eae
Ludea <ludealol.olol@gmail.com>
2026-04-02 19:38:33 +0000

    fix: verify type remote module

--- src/components/Plugins.tsx
+++ src/components/Plugins.tsx
@@ -1,5 +1,5 @@
 import { ModuleFederation } from "@module-federation/runtime";
-import { useState, useEffect, useContext, FC, ReactElement } from "react";
+import { useState, useEffect, useContext, ReactElement, ComponentType } from "react";
 import { SparusErrorContext } from "utils/Context";
 
 type PluginPosition = "header" | "body" | "footer";
@@ -9,11 +9,17 @@ type PluginsProps = {
   register: (pos: PluginPosition, el: ReactElement) => void;
 };
 
+type RemoteModule = {
+  default: ComponentType<PluginsProps>;
+};
+
+function isRemoteModule(mod: unknown): mod is RemoteModule {
+  return typeof mod === "object" && mod != null && "default" in mod;
+}
+
 export const Plugins = ({ path, register, mf }: PluginsProps & { mf: ModuleFederation }) => {
   const { setGlobalError } = useContext(SparusErrorContext);
-  const [Comp, setComponent] = useState<FC<{
-    register: typeof register;
-  }> | null>(null);
+  const [Comp, setComponent] = useState<ComponentType<PluginsProps> | null>(null);
 
   useEffect(() => {
     const base_url = "http://localhost:8012/plugins/";
@@ -26,12 +32,19 @@ export const Plugins = ({ path, register, mf }: PluginsProps & { mf: ModuleFeder
     ]);
 
     mf.loadRemote(path + "/Button")
-      .then((mod) => setComponent(() => mod.default))
+      .then((mod) => {
+        if (!isRemoteModule(mod)) {
+          setGlobalError("invalid plugin");
+          return;
+        }
+        setComponent(mod.default);
+      })
       .catch((err: unknown) => {
+        console.log("err", err);
         setGlobalError(err);
       });
   }, [path]);
 
   if (!Comp) return null;
-  return <Comp register={register} />;
+  return <Comp path={path} register={register} />;
 };
